c254f1b239eff1e2f109ea559d04bd08cff91190
‘Juliano <julianoperins@gmail.com>
2020-06-05 04:54:37 -0400

    Set up methods for the form

--- src/App.js
+++ src/App.js
@@ -12,14 +12,51 @@ const initialExpense = [
 ];
 
 const App = () => {
+  //! ************ State Values  *****************/
+  //! all expenses, add expense
   const [expenses, setExpenses] = useState(initialExpense);
+
+  //! single expense
+  const [charge, setCharge] = useState("");
+  console.log(charge);
+
+  //! single amount
+  const [amount, setAmount] = useState("");
+  console.log(amount);
+
+  //! ************ Functionality  *****************/
+  const handleCharge = (e) => {
+    setCharge(e.target.value);
+  };
+
+  const handleAmount = (e) => {
+    setAmount(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (charge !== "" && amount > 0) {
+      const singleExpense = { id: uuidv4(), charge, amount };
+      setExpenses([...expenses, singleExpense]);
+      setCharge("");
+      setAmount("");
+    } else {
+      //! Handle alert call
+    }
+  };
 
   return (
     <>
       <Alert />
       <h1>Budget Calculator:</h1>
       <main className="App">
-        <ExpenseForm />
+        <ExpenseForm
+          charge={charge}
+          amount={amount}
+          handleCharge={handleCharge}
+          handleAmount={handleAmount}
+          handleSubmit={handleSubmit}
+        />
         <ExpenseList expenses={expenses} />
       </main>
       <h1>
@@ -27,7 +64,7 @@ const App = () => {
         <span className="total">
           $
           {expenses.reduce((acc, curr) => {
-            return (acc += curr.amount);
+            return (acc += parseInt(curr.amount));
           }, 0)}
         </span>
       </h1>
--- src/components/ExpenseForm.js
+++ src/components/ExpenseForm.js
@@ -1,28 +1,38 @@
 import React from "react";
 import { MdSend } from "react-icons/md";
 
-const ExpenseForm = () => {
+const ExpenseForm = ({
+  charge,
+  amount,
+  handleCharge,
+  handleAmount,
+  handleSubmit,
+}) => {
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <div className="form-center">
         <div className="form-group">
-          <label htmlFor="charge">Charge</label>
+          <label htmlFor="charge">Expense description:</label>
           <input
             type="text"
             className="form-control"
             id="charge"
             name="charge"
             placeholder="e.g. rent..."
+            value={charge}
+            onChange={handleCharge}
           />
         </div>
         <div className="form-group">
-          <label htmlFor="amount">Amount</label>
+          <label htmlFor="amount">Amount:</label>
           <input
-            type="text"
+            type="number"
             className="form-control"
             id="amount"
             name="amount"
-            placeholder="e.g. rent..."
+            placeholder="e.g. 100..."
+            value={amount}
+            onChange={handleAmount}
           />
         </div>
       </div>
