2343264e5659335c8b2eb33226c358108e62a75a
‘Juliano <julianoperins@gmail.com>
2020-06-05 02:00:08 -0400

    Applied reduce method to the array of elements

--- src/App.js
+++ src/App.js
@@ -1,17 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 import ExpenseList from "./components/ExpenseList";
 import ExpenseForm from "./components/ExpenseForm";
 import Alert from "./components/Alert";
+import { v4 as uuidv4 } from "uuid";
 
-function App() {
+const initialExpense = [
+  { id: uuidv4(), charge: "rent", amount: 1600 },
+  { id: uuidv4(), charge: "car payment", amount: 400 },
+  { id: uuidv4(), charge: "credit card payment", amount: 1200 },
+];
+
+const App = () => {
+  const [expenses, setExpenses] = useState(initialExpense);
+
   return (
     <>
       <Alert />
-      <ExpenseForm />
-      <ExpenseList />
+      <h1>Budget Calculator:</h1>
+      <main className="App">
+        <ExpenseForm />
+        <ExpenseList expenses={expenses} />
+      </main>
+      <h1>
+        total spending:{" "}
+        <span className="total">
+          $
+          {expenses.reduce((acc, curr) => {
+            return (acc += curr.amount);
+          }, 0)}
+        </span>
+      </h1>
     </>
   );
-}
+};
 
 export default App;
