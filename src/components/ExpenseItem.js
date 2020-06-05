734d103d77a6bce3ebe47fcf0f63e89f77184393
‘Juliano <julianoperins@gmail.com>
2020-06-05 00:37:38 -0400

    first commit

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ExpenseItem = () => {
+  return <div>expenseItem</div>;
+};
+
+export default ExpenseItem;
---
+++ src/components/ExpenseList.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Item from "./ExpenseItem";
+
+const ExpenseList = () => {
+  return (
+    <div>
+      <Item />
+    </div>
+  );
+};
+
+export default ExpenseList;
